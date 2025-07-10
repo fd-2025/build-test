0664a4fdde35dd0b852c67edbab61820fd75acc7
fd-2025 <217896674+fd-2025@users.noreply.github.com>
2025-07-10 17:36:36 +0200

    test docker 12 full build

--- setup-volume.sh
+++ setup-volume.sh
@@ -5,6 +5,15 @@ pwd
 ls -ld .
 ls -la
 
+echo "PATH: $PATH"
+echo "ANDROID_HOME: $ANDROID_HOME"
+echo "ANDROID_SDK_ROOT: $ANDROID_SDK_ROOT"
+echo "ANDROID_SDK: $ANDROID_SDK"
+echo "ANDROID_NDK: $ANDROID_NDK"
+echo "FDROIDSERVER: $FDROIDSERVER"
+echo "PYTHONPATH: $PYTHONPATH"
+echo "PYTHONUNBUFFERED: $PYTHONUNBUFFERED"
+
 git clone https://gitlab.com/f-droid-mirrors/OsmAnd-submodules.git
 pushd OsmAnd-submodules
 git submodule init
